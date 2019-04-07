1cd2b1462615f76f45b81808e9d760a003deded4
Kaspars <smilga.kaspars@gmail.com>
2019-04-07 13:05:07 +0300

    set 18 workers

--- puppeteer/config.js
+++ puppeteer/config.js
@@ -5,7 +5,7 @@ const gotoConf = {
     waitUntil: 'networkidle2',
 };
 
-const WORKERS = 12;
+const WORKERS = 18;
 
 const UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36';
 
